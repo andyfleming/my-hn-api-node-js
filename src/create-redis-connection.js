be976cf8da97edb630ba000219a417492f3025cd
Andy Fleming <andy@andyfleming.com>
2017-11-19 22:18:04 -0800

    Adding environment variables for redis

--- src/create-redis-connection.js
+++ src/create-redis-connection.js
@@ -2,9 +2,9 @@ const Redis = require('ioredis')
 
 module.exports = function createRedisConnection() {
     return Promise.resolve(new Redis({
-        port: 7855,
-        host: '127.0.0.1',
-        password: 'ahr098gi3refjd',
+        port: process.env.REDIS_PORT || 7855,
+        host: process.env.REDIS_HOST || '127.0.0.1',
+        password: process.env.REDIS_PASS || 'ahr098gi3refjd',
         db: 0
     }))
 }
